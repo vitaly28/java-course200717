8e66a29b89fe5def7423f5003eab8ebb314eef98
Vitaly Berstenev <sm_plus82@mail.ru>
2017-07-20 16:47:09 +0400

    vitaly add

---
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -0,0 +1,18 @@
+package job4j.Calculate;
+
+/**
+ * Calculate.
+ *
+ * @author Vitaly Berstenev(sm_plus82@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Calculate {
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
